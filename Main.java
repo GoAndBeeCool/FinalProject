3b918e61e5f4c599e12131fc30e7c7c00a07d8d5
yikaip <ypeng@uwyo.edu>
2018-12-15 23:47:27 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub			
 		BinaryTree tree = new BinaryTree();
-		System.out.print("============Binary Tree==========");
+		System.out.print("============Binary Tree============");
 		tree.askForRawEvent();
 		tree.printLevelOrder();
 
